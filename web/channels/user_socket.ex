218caf7d90349759315eb41b040ff7bfd2864395
denispeplin <denis.peplin@gmail.com>
2016-07-31 20:29:36 +0300

    Implement sample chat app.

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule HelloPhoenix.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "rooms:*", HelloPhoenix.RoomChannel
+  channel "room:*", HelloPhoenix.RoomChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
